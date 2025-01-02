a61bf93521551dc37ac71e4a673b845c2de787cd
Kishore <kishore22705@gmail.com>
2025-01-02 17:21:09 +0530

    First

---
+++ rag.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, jsonify
+import os
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hello():
+    return jsonify({'message': 'Hello World!'})
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
